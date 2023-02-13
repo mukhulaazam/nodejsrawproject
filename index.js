eb3fdd1b0d4f9486a276ad1fbd6ec3b25235e734
mukhulaazam <mukhulaazam@gmail.com>
2023-02-14 00:08:18 +0600

    create file done next is read file data

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const http = require('http');
 const { helperReqRes } = require('./helpers/helper');
 const env = require('./helpers/environments');
 
+const createData = require('./lib/data');
+
 // module scaffolding
-const app ={}
+const app = {}
+
+createData.create('createTest', 'createFileStore', { name: 'Mamun', age: 25 }, (err) => {
+    console.log('error was:',err)
+})
 
 // create server
 app.createServer = () => {
-    const server = http.createServer(app.handleReqRes)
+    const server = http.createServer(app.handleReqRes) 
     server.listen(env.port, () => {
         console.log(`Server Listening to port: ${env.port}`)
     })
