72e10db2bf4b715c882561ed1d9552843ec18f28
mukhulaazam <mukhulaazam@gmail.com>
2023-02-19 23:48:29 +0600

    user crud started

--- handlers/routeHandler/userRouteHandler.js
+++ handlers/routeHandler/userRouteHandler.js
@@ -1,10 +1,16 @@
 const handler = {};
 
-handler.aboutHandler = (data, callback) => {
-    console.log(data)
-    callback(200, {
-        message: 'This is User List page',
-    });
+handler.userHandler = (reqProperties, callback) => {
+    const allowedMethods = ['get', 'post', 'put', 'delete'];
+    if (allowedMethods.indexOf(reqProperties.method) > -1) {
+    } else {
+        callback(405);
+    }
 }
+handler._users = {};
 
+handler._users.get = (reqProperties, callback) => { };
+handler._users.post = (reqProperties, callback) => { };
+handler._users.put = (reqProperties, callback) => { };
+handler._users.delete = (reqProperties, callback) => { };
 module.exports = handler;
